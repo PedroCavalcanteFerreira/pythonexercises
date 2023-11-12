0093e741f37fd41cd838790c65dd65b8e3d6cc74
Pedro Cavalcante <cf.pedro2008@gmail.com>
2023-11-12 16:03:21 -0300

    Correção feita em 7_cinema.py

--- 7_cinema.py
+++ 7_cinema.py
@@ -4,26 +4,30 @@
 data = input("\nDatas disponíveis:\n     Domingo | Segunda | Terça | Quarta | Quinta | Sexta | Sábado\nInforme o dia da semana para a compra do(s) ingresso(s): ")
 quantidade = int(input("Informe a quantidade de ingressos: "))
 tipos = []
-print("Tipos de ingressos:\n     Junior: R$25.00 | Standart: R$40.00 | Senior: R$ 20.00\n")
+print("Tipos de ingressos:\n     Junior(até 12 anos): R$25.00 | Standart(de 13 a 61 anos): R$40.00 | Senior(acima de 61 anos): R$ 20.00\n")
 for i in range(quantidade):
-    tipos.append(input(f"Informe o tipo do ingresso {i+1}: "))
+    tipos.append(int(input(f"Informe a idade de quem usara o ingresso {i+1}: ")))
 
 valor = {"junior": 25.00, "standart": 40.00, "senior": 20.00}
 desconto = 0
 sub_ju = 0
 sub_stan = 0
 sub_sen = 0
+stan = 0
 def sub():
     global sub_ju, sub_stan, sub_sen
     for i in tipos:
-        if i.lower() == 'junior':
+        if i <= 12:
             sub_ju += valor["junior"]
-        if i.lower() == "standart":
+        if 12 < i < 61:
             sub_stan += valor["standart"]
-        if i.lower() == "senior":
+        if i >= 61:
             sub_sen += valor["senior"]
 if data.lower() == "segunda":
-    desconto = 20 * tipos.count("standart")
+    for i in tipos:
+        if 12 < i < 61:
+            stan += 1
+    desconto = 20 * stan
 
 sub()
 print(f"\nSubtotal Junior: R${sub_ju:.2f}")
